707093867c72edd3d54d2014b06d185612ddd881
ayaka.nishihara <ayaka19870114@yahoo.co.jp>
2021-07-09 05:20:57 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,7 +10,10 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item">{!! link_to_route('tasks.create', 'タスクの追加', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ユーザ登録ページへのリンク --}}
+                <li>{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ログインページへのリンク --}}
+                <li><a href="#">ログイン</a></li>
             </ul>
         </div>
     </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -25,6 +25,6 @@
         </table>
     @endif
     
-    {!! link_to_route('tasks.create', 'タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規追加', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
